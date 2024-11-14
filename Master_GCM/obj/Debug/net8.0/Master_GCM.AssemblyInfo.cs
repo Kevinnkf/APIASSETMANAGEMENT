0850901fa3c43ba3cbd5c86da684cf6f34f49af1
Kevin <khalfanifadillah.com>
2024-11-14 09:54:23 +0700

    Tested update picture

--- Master_GCM/obj/Debug/net8.0/Master_GCM.AssemblyInfo.cs
+++ Master_GCM/obj/Debug/net8.0/Master_GCM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af26b5c8474a1f16dd8ee71eb4a3a44073546515")]
 [assembly: System.Reflection.AssemblyProductAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
